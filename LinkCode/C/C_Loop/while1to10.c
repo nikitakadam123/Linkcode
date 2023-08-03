4797c4e400289dab6ad9ea86854d02f592976aad
Nikita Kadam <114174410+nikitakadam123@users.noreply.github.com>
2023-08-03 00:09:52 -0700

    Add files via upload

---
+++ LinkCode/C/C_Loop/while1to10.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+	int i,n;
+	i=1;
+	while(i<=10)
+	{
+		printf("\n%d",i);
+		i++;
+		
+	}
+	printf("\n Thankyou...");
+	
+	n=10;
+	while(n>=1)
+	{
+		printf("\n%d",n);
+		n--;
+	}
+	
+	return 0;
+}
